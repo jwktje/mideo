272b8f40cc0f0ca12621c914c2e31fe1b1ca5cef
jwktje <jwkolkman@gmail.com>
2019-01-09 23:32:14 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
     entry: {
         client: './app/js/client/main.js',
-        //admin: './app/js/admin/main.js',
     },
     output: {
         path: path.join(__dirname, "app/dist/js"),
